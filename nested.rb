7d1b257a0fb938e1808d20f3c8dd504497e09c9f
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-08 06:09:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,9 +107,11 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:alans_new_info] = known_for.delete: "GUI"
+     #alans_new_info = "GUI"
+     programmer_hash[:alan_kay].delete(:known_for)
+     programmer_hash[:alan_kay][:alans_new_info] = "GUI"
      
+     return programmer_hash
 end
 
 def adding_to_dennis
