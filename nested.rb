5721a53773eb4c46f383cf7843b05175e2f78003
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-08 05:51:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,6 +86,7 @@ def adding_matz
     :known_for => "Ruby",
       :languages => ["LISP", "C"]
   }
+  
   return programmer_hash
 end
 
@@ -107,7 +108,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:ala]
      
 end
 
